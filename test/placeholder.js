27de394a7ee4b3d26b82db3d927784de9f603d37
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2011-01-17 17:54:54 +0100

    implement placeholder

---
+++ test/placeholder.js
@@ -0,0 +1,19 @@
+QUnit.module('placeholder');
+
+test('replace placeholder in code', 1, function() {
+    var code = '{VER} exports.test = 123;\n var test = "test";\n {COPYRIGHT}',
+        o = {
+            processors: {
+                placeholder: {
+                    'VER': '0.1.2',
+                    'COPYRIGHT': 'some text'
+                }
+            }
+        };
+        
+    equal(
+        run('test/test.js', code, o),
+        '0.1.2 exports.test = 123;\n var test = "test";\n some text',
+        'placeholder works'  
+    );
+});
