f3c99144d4ad7ef3d2de7528edf2f56daae62252
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2011-01-19 15:30:22 +0100

    add remote url support for dataimg processor

--- test/dataimg.js
+++ test/dataimg.js
@@ -4,7 +4,7 @@ var fixtures = __dirname + '/fixtures/dataimg';
 
 var sample = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABRElEQVR42mNgAIKAuIb/AXH1cMyABwTC1Tb8B7EZYAaAwMytR/4fuv7of2B8A1ZDUNRde4RqGUgTTDIgDrcBMEuwqgEJHjx26390yeL/EG81gF0TCHUuSHzPweu4vRmc0vUf5kxs4PT5B2D54NQuTANCU3vyi1vW/ycG1PVu+e8f2+iLGrpAp+48cB2sYNvpx/8rZu1C0VQxc9f/KVsfgNnL1p3GDGiQv3YfghjgGJQFkgSzGaAMU+cIuNiKDWcwAzEwqTUurWYlxDag7SBXgEDL5J1wV6V37gCzcxvXAL1Q54s1Fmq6N6M43SGgCIVf3rERf2IDScLAoZsfUGgQwJVGUFIajK7q3ATxxiSI0yugfKwpFT0loiciEBvkOpA8hiEwZ8PSeGReP1ZngjSB5GGGwMMCOTcGQm3H5UXk3Ig3PIgFALVQ3pXPHoX7AAAAAElFTkSuQmCC";
 
-test('simple', 2, function() {
+test('simple', 3, function() {
     var o = {
         processors: {
             dataimg: true    
@@ -21,4 +21,7 @@ test('simple', 2, function() {
     res = run('/test.css', src, o);
     equal('a { background: url("/ape.jpg") }', res, 'image is not encoded because its too large');
    
+    src = 'a { background: url(http://ape.jpg) }'
+    res = run('/test.css', src, o);
+    equal('a { background: url("http://ape.jpg") }', res, 'image is not encoded because its remote');   
 });
