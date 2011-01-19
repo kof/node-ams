6219adbf9bca848573f314d14a86128f7837fe78
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2011-01-19 12:36:18 +0100

    rewrite vendorcss processor, now its regex based

--- test/vendorcss.js
+++ test/vendorcss.js
@@ -4,12 +4,18 @@ var inspect = require('util').inspect;
 
 test('replace', function() {
     
-    var ocss = 'a { box-shadow: #000 5px 3px 5px; }';
+    var ocss = 'a { box-shadow: #000 5px 3px 5px; font-size: 12px}';
     
-    expcss = 'a {\n  box-shadow: #000 5px 3px 5px;\n  -moz-box-shadow: #000 5px 3px 5px;\n  -webkit-box-shadow: #000 5px 3px 5px;\n  -o-box-shadow: #000 5px 3px 5px;\n  -ms-box-shadow: #000 5px 3px 5px;\n}\n';
-    
+    expcss = 'a {\
+                box-shadow: #000 5px 3px 5px;\
+                -moz-box-shadow: #000 5px 3px 5px;\
+                -webkit-box-shadow: #000 5px 3px 5px;\
+                -o-box-shadow: #000 5px 3px 5px;\
+                -ms-box-shadow: #000 5px 3px 5px;\
+                font-size: 12px;\
+              }'.replace(/\s/g, '');
     equal(
-        run('/test/test.css', ocss),
+        run('/test/test.css', ocss).replace(/\s*/g, ''),
         expcss,
         'vendor css are replaced'  
     );
