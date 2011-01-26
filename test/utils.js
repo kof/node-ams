85e9ecf698b165d995487a87888d48c2aceacac1
Oleg Slobodskoi @oleg008 <oleg008@gmail.com>
2011-01-26 18:14:39 +0100

    added rmSync utility, build cleans up the target dir before to write

--- test/utils.js
+++ test/utils.js
@@ -4,10 +4,12 @@ var n = require('../deps/natives');
 
 var fixtures = n.path.join(__dirname, 'fixtures');
 
-test('findSync', 3, function() {
-   var root = n.path.join(fixtures, 'utils');
+var tmp = n.path.join(__dirname, 'tmp');
 
-   same( findSync(root), [root + '/a.js', root + '/b.js'], 'no regexp no rec');
+test('findSync', 3, function() {
+   var root = fixtures + '/utils';
+
+   same( findSync(root), [root + '/a', root + '/a.js', root + '/b.js'], 'no regexp no rec');
    same( 
        findSync(root, /\.js$/), 
        [root + '/a.js', root + '/b.js'], 
@@ -22,23 +24,21 @@ test('findSync', 3, function() {
 });
 
 test('mkdirSync', 2, function() {
-    var dir1 = n.path.join(__dirname, 'tmp', 'test');
+    var dir1 = tmp + '/test';
     
     try {
         mkdirSync(dir1);
         n.fs.statSync(dir1);
-        n.fs.rmdirSync(dir1);    
         ok(true, 'dir '+dir1+' was created');
     } catch(e) {
         ok(false, 'create '+dir1+' failed');
     }
 
-    dir2 = n.path.join(dir1, 'test');
+    var dir2 = tmp + '/test/test';
+
     try {
         mkdirSync(dir2);
         n.fs.statSync(dir2);
-        n.fs.rmdirSync(dir2);    
-        n.fs.rmdirSync(dir1);    
         ok(true, 'dir '+dir2+' was created');
     } catch(e) {
         ok(false, 'create '+dir2+' failed');
@@ -48,24 +48,28 @@ test('mkdirSync', 2, function() {
 
 test('copySync', 1, function() {
    
-   var target = n.path.join(__dirname, 'tmp'),
-       src = n.path.join(fixtures, 'utils'); 
+   var src = fixtures + '/utils'; 
     
    try {
-       copySync(src, target);
-       n.fs.statSync(target + '/a.js');
-       n.fs.statSync(target + '/b.js');
-       n.fs.statSync(target + '/a/c.js');
-       // cleanup
-       n.fs.unlinkSync(target + '/a.js');
-       n.fs.unlinkSync(target + '/b.js');
-       n.fs.unlinkSync(target + '/a/c.js');
-       n.fs.rmdirSync(target + '/a');
-
+       copySync(src, tmp);
+       n.fs.statSync(tmp + '/a.js');
+       n.fs.statSync(tmp + '/b.js');
+       n.fs.statSync(tmp + '/a/c.js');
        ok(true, 'dir '+src+' was copied');
    } catch(e) {
        console.error(e);
        ok(false, 'dir '+src+' was copied');
    }
 });
-    
+
+test('rmSync', function(){
+   var done = false; 
+
+   try {
+       rmSync(tmp);
+       done = true; 
+   } catch(e) {
+       console.error(e);
+   }
+   ok(done, 'all files removed');
+});
