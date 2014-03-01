86d682ab77862378c364f0ae4b683bf7aa5372d6
Oleg Slobodskoi <oleg008@gmail.com>
2014-03-01 23:55:17 +0100

    update uglify

--- test/uglifyjs.js
+++ test/uglifyjs.js
@@ -2,7 +2,7 @@ QUnit.module('uglifyjs');
 
 test('minify js code', 1, function() {
     equal(
-        run('test/test.js', 'exports.test = 123;\n var test = "test";\n', {}),
+        run('test/test.js', 'exports.test = 123;\n var test = "test";\n', {fromString: true}),
         'exports.test=123;var test="test";',
         'minifier works'
     );
